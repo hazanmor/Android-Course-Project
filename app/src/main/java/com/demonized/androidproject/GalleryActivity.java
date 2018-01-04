978b54d390cf365d2873c1c9e344644fd0cd580b
morhaz <hazan2101@gmail.com>
2018-01-04 15:57:50 +0200

    Speech2Text initial

--- app/src/main/java/com/demonized/androidproject/GalleryActivity.java
+++ app/src/main/java/com/demonized/androidproject/GalleryActivity.java
@@ -1,13 +1,115 @@
 package com.demonized.androidproject;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class GalleryActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class GalleryActivity extends AppCompatActivity implements RecognitionListener{
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(this);
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
+                "en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                this.getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        speech.startListening(recognizerIntent);
+        SpeechThread speechThread = new SpeechThread();
+        speechThread.start();
+
+
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onRmsChanged(float v) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] bytes) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int i) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        for (String result : matches)
+            text += result + "\n";
+        ((TextView)findViewById(R.id.keywords)).setText(text);
+    }
+
+    @Override
+    public void onPartialResults(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onEvent(int i, Bundle bundle) {
+
+    }
+
+    class SpeechThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            try{
+                sleep(5000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    speech.stopListening();
+                }
+            });
+            try{
+                sleep(5000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    speech.startListening(recognizerIntent);
+                }
+            });
+        }
     }
 }
